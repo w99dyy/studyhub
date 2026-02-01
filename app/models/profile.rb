aed7fa010b01e1623bb8255ed7d01dbf42268b5b
Huzaifa <huzskywalker@proton.me>
2026-02-02 00:16:25 +0200

    RuboCop spaces

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,14 +2,13 @@ class Profile < ApplicationRecord
   belongs_to :user
   validates :username, presence: true, uniqueness: true
   validates :bio, length: { maximum: 500 }
-  
+
   before_validation :set_default_username, on: :create
-  
+
 
   private
-  
+
   def set_default_username
     self.username ||= "user#{User.count + 1000}" # or use email prefix
   end
 end
-
